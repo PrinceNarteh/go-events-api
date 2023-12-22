89f27d6a6af5652bb7ca9ea8f66dc77198bd479d
Prince Narteh <prinarteh@gmail.com>
2023-12-22 15:00:20 +0000

    minor changes

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/PrinceNarteh/go-events-api/controllers"
+	"github.com/PrinceNarteh/go-events-api/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", controllers.GetEvents)
 	server.POST("/events", controllers.CreateEvent)
